d32323c11e4ae35ef88ff15b437fd3a436933b0a
marsal.sans <marsal.sans@gmail.com>
2024-05-25 17:17:41 +0200

    husky test

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -2,6 +2,7 @@ module.exports = {
     extends: ['eslint:recommended', 'plugin:prettier/recommended'],
     plugins: ['prettier'],
     rules: {
+        'no-unused-vars': 'off', // Desactivamos la regla no-unused-vars
         // Aquí puedes agregar o modificar reglas específicas de ESLint si lo deseas
     },
 };
