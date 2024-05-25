2a2436aec6720a8e905c41eb4727326158efedaa
marsal.sans <marsal.sans@gmail.com>
2024-05-25 17:15:18 +0200

    husky test

---
+++ eslint.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+    extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+    plugins: ['prettier'],
+    rules: {
+        // Aquí puedes agregar o modificar reglas específicas de ESLint si lo deseas
+    },
+};
