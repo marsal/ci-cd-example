284ba7c4635aa58877fbf62e36fb8b1ca4b10784
marsal.sans <marsal.sans@gmail.com>
2024-05-27 15:36:57 +0200

    test

---
+++ frontend/test/counter.spec.tsx
@@ -0,0 +1,45 @@
+import {
+  act,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from '@testing-library/react';
+import fetchMock from 'fetch-mock';
+import App from '../src/App';
+
+describe('App', () => {
+  it('Renders correctly and buttons works', async () => {
+    fetchMock.get('http://localhost:5173/api/counter', { id: 1, count: 0 });
+    fetchMock.get('http://localhost:5173/api/counter/1/increase', {
+      id: 1,
+      count: 1,
+    });
+    fetchMock.get('http://localhost:5173/api/counter/1/decrease', {
+      id: 1,
+      count: -1,
+    });
+
+    render(<App />);
+
+    await waitFor(() => {
+      expect(screen.getByText('count is 0')).toBeInTheDocument();
+    });
+
+    await act(() => {
+      fireEvent.click(screen.getByText('Increase'));
+    });
+
+    await waitFor(() => {
+      expect(screen.getByText('count is 1')).toBeInTheDocument();
+    });
+
+    await act(() => {
+      fireEvent.click(screen.getByText('Decrease'));
+    });
+
+    await waitFor(() => {
+      expect(screen.getByText('count is -1')).toBeInTheDocument();
+    });
+  });
+});
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,17 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    server: {
-        proxy: {
-            '/api': 'http://localhost:3000',
-        },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000',
     },
-    plugins: [react()],
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './test/setup.ts',
+  },
+  plugins: [react()],
 });
