dc71a6149916434399c0b9cd8002a02a8826eb34
marsal.sans <marsal.sans@gmail.com>
2024-05-27 17:07:51 +0200

    test

--- backend/test/integration/app.steps.ts
+++ backend/test/integration/app.steps.ts
@@ -4,6 +4,7 @@ import { AppModule } from 'src/app.module';
 import { PrismaCounterRepository } from 'src/counter/infraestructure/prismaCounter.repository';
 import { World } from './World';
 import { InMemoryCounterRepository } from './inMemoryCounter.repository';
+import { PrismaService } from 'src/common/prisma.service';
 
 Before({ timeout: 10 * 1000 }, async function (this: World) {
   const moduleFixture = await Test.createTestingModule({
@@ -12,6 +13,8 @@ Before({ timeout: 10 * 1000 }, async function (this: World) {
     .overrideProvider('CounterRepository')
     .useClass(InMemoryCounterRepository)
     .overrideProvider(PrismaCounterRepository)
+    .useValue(null)
+    .overrideProvider(PrismaService)
     .useValue(null)
     .compile();
 
