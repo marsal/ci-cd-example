7a89fb452659bbd09fa6020c643ba4b284ecf79e
marsal.sans <marsal.sans@gmail.com>
2024-05-28 13:40:22 +0200

    Test deploy docker

--- build/scripts/deploy.sh
+++ build/scripts/deploy.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+# Imprimir un mensaje para indicar que el script está comenzando
+echo "Iniciando el script deploy.sh"
+
 # Cambiar al directorio ci-cd-example
+echo "Cambiando al directorio /root/ci-cd-example"
 cd /root/ci-cd-example
 
 # Detener los servicios con docker-compose
+echo "Deteniendo los servicios con docker-compose"
 docker-compose -f docker-compose-deploy.yml down
 
 # Actualizar el repositorio git
+echo "Actualizando el repositorio git"
 git pull
 
 # Iniciar los servicios con docker-compose
+echo "Iniciando los servicios con docker-compose"
 docker-compose -f docker-compose-deploy.yml up -d --force-recreate
+
+# Imprimir un mensaje para indicar que el script ha terminado
+echo "Script deploy.sh completado"
